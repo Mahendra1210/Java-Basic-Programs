599dbb74408d2efd5ebf0cabc67538ee26c55c57
Mahendra patel <Mahendra patel@HP-15s>
2022-11-04 20:30:29 +0530

    3 commit

---
+++ Basic Java Programs/src/com/basic/programs/Sum_of_n_natural_numbers.java
@@ -0,0 +1,22 @@
+package com.basic.programs;
+import java.util.*;
+
+public class Sum_of_n_natural_numbers {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); //for user input
+		
+		System.out.print("Enter the value of n : ");
+		int n = sc.nextInt();
+		int sum=0;
+		
+		//for loop
+		for(int i=1; i<=n; i++) {
+			sum = sum+i;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
