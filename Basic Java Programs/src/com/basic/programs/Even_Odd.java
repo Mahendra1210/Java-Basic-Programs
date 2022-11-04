d1cd57841c68aea65f59f9098ab66d2e3fe9b38a
Mahendra patel <Mahendra patel@HP-15s>
2022-11-04 18:22:23 +0530

    1 commit

---
+++ Basic Java Programs/src/com/basic/programs/Even_Odd.java
@@ -0,0 +1,20 @@
+package com.basic.programs;
+import java.util.*;
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int num = sc.nextInt();
+		
+		if(num % 2 == 0) {
+			System.out.println("Even number");
+		} else {
+			System.out.println("Odd number");
+		}
+		sc.close();
+	}
+
+}
