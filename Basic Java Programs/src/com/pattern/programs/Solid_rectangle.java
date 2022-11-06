5bcbf98c3fc76fab0b4f43aaf74637c64d35ec10
Mahendra patel <Mahendra patel@HP-15s>
2022-11-06 12:22:31 +0530

    1 commit

---
+++ Basic Java Programs/src/com/pattern/programs/Solid_rectangle.java
@@ -0,0 +1,21 @@
+package com.pattern.programs;
+
+public class Solid_rectangle {
+
+	public static void main(String[] args) {
+	
+		int n = 4;
+		int m = 5;
+		
+		//outer for loop
+		 for(int i=1; i<=n; i++) {
+			 //inner for loop
+	         for(int j=1; j<=m; j++) {
+	             System.out.print("*");
+	         }
+	         System.out.println("");
+	        
+	      }
+	}
+
+}
