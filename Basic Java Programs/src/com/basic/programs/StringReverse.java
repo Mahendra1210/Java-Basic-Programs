e50df7f1d263ff896d4ba20e94824cbe86fc7820
Mahendra patel <Mahendra patel@HP-15s>
2022-12-03 20:01:53 +0530

    13 commit

---
+++ Basic Java Programs/src/com/basic/programs/StringReverse.java
@@ -0,0 +1,21 @@
+package com.basic.programs;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		StringBuilder name = new StringBuilder("Patel");
+		
+		for(int i=0;i<=name.length()/2;i++) {
+			int front = i; 
+			int back = name.length()-1-i;
+			
+			char frontchar = name.charAt(front);
+			char backchar = name.charAt(back);
+			
+			name.setCharAt(front, backchar);
+			name.setCharAt(back, frontchar);
+		}
+		System.out.println(name);
+	}
+
+}
